626d6f704dc6163f2f7daa02a5ed051533598d8c
Joa <jgfontes9@gmail.com>
2024-02-16 18:17:07 -0300

    Create entities and enums.

---
+++ Entities/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliver.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime date)
+        {
+            Name = name;
+            Email = email;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Name} ({Date.ToString("dd/MM/yyyy HH:mm:ss")}) - {Email}");
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deliver.Entities.Enums;
+
+namespace Deliver.Entities
+{
+    internal class Order
+    {
+        public DateOnly Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Clt { get; set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+
+        public Order() 
+        { 
+        }
+
+        public Order(DateOnly moment, OrderStatus status, Client clt)
+        {
+            Moment = moment;
+            Status = status;
+            Clt = clt;
+        }
+
+        public void AddOrder(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveOrder(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum = item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY");
+            sb.Append("Order Moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.AppendLine($"Client: {Clt}");
+            sb.AppendLine("Order items: ");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine($"{item.Product}, ${item.Price}, Quantity: {item.Quantity}, Subtotal: ${item.SubTotal()}");
+            }
+            sb.AppendLine($"Total price: ${Total()}");
+
+            return sb.ToString();
+        }
+    }
+}
